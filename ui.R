0b7aeb0f3ef2542a68f57a1183312e83c5737ae7
apanarun <34540405+apanarun@users.noreply.github.com>
2020-05-20 22:32:00 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ fluidPage(
       
      
      # Define the sidebar with one input 
-     sidebarPanel(p(strong("Documentation:",style="color:red"), a("User Help Page",href="http://rpubs.com/joanperez/293126")), 
+     sidebarPanel(p(strong("Documentation:",style="color:red"), a("User Help Page",href="https://rpubs.com/apanarun/617286")), 
        selectInput("Region", "Region:",  
                    choices=colnames(prpeople)), 
        hr(), 
